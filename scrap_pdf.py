8e000c5acfb0da177da28c49bbe277c375b3701c
hiki270bis <hiki270bis>
2021-03-16 11:55:06 +0100

    added count pages function and a default pdf icon

--- scrap_pdf.py
+++ scrap_pdf.py
@@ -2,7 +2,8 @@ import re
 import requests
 import io
 import pdfplumber
-
+from PIL import Image
+from time import time
 
 
 class PDFScraper():
@@ -12,6 +13,7 @@ class PDFScraper():
         self.lines = self.return_first_lines(self.file_io)
         self.img = self.return_img(self.file_io)
         self.words = self.return_words(self.file_io)
+        self.pages = self.return_pages(self.file_io)
 
     # If the url provided takes directly to the pdf file, that is used as self.url
     # else this method searches for a link to the file and uses that as self.url
@@ -47,11 +49,17 @@ class PDFScraper():
         with pdfplumber.open(file) as pdf:
             first_page = pdf.pages[0]
             if not first_page.images:
-                img = None
-                print(img)        # Return a default image of a pdf if there is no usable img on the first page
+                img = Image.open(
+                    "pdf_img.png")  # Return a default image of a pdf if there is no usable img on the first page
             else:
                 img = first_page.images[0]
         return img
+
+    # Returns number of pdf pages
+    def return_pages(self, file):
+        with pdfplumber.open(file) as pdf:
+            n_pages = len(pdf.pages)
+         return n_pages
 
     # Returns n° of word in pdf using bytes stream
     def return_words(self, file):
@@ -64,6 +72,30 @@ class PDFScraper():
 
 
 if __name__ == "__main__":
-    test_url = "http://www.axmag.com/download/pdfurl-guide.pdf"
-    test_pdf = PDFScraper(test_url)
-    print(test_pdf.img)
+    test_url = ["https://dash.harvard.edu/bitstream/handle/1/3403038/darnton_historybooks.pdf",
+                "http://www.axmag.com/download/pdfurl-guide.pdf",
+                "https://www.research.gov/common/attachment/Desktop/How_do_I_create_a_PDF-A_file.pdf",
+                "https://library.princeton.edu/special-collections/sites/default/files/Creating_PDFA.pdf",
+                "http://www.umass.edu/preferen/gintis/hypercognition.pdf",
+                "https://www.researchgate.net/publication/315905287_INTRODUCTION_TO_ANTHROPOLOGY",
+                "https://theologicalstudies.org.uk/pdf/anthropology_cameron.pdf",
+                "https://www.researchgate.net/publication/327430054_Business_Anthropology",
+                "http://ijhssnet.com/journals/Vol_4_No_10_1_August_2014/19.pdf",
+                "http://marcuse.faculty.history.ucsb.edu/classes/201/articles/78KelleyPublicHistoryOriginsTPH0001.pdf"]
+    t0 = time()
+    for url in test_url:
+        try:
+            test_pdf = PDFScraper(url)
+            print(test_pdf.img)
+            print(test_pdf.url)
+            print(test_pdf.lines)
+            print(test_pdf.pages)
+        except:
+            print(f"unable to retrieve {url}")
+    t1 = time()
+    print(f"it took {t1 - t0} secs")
+
+
+# counting words: used 10 random PDFs; some with 500 words, others with 10000+. It took 109 secs
+# counting pages: used 10 random PDFs. It took 45 secs
+# counting neither pages nor words, using the same 10 random PDFs, took 42 secs
